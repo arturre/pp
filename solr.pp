e4f332765dc5528d5990a2bdf901e0a2947aa197
arturre <artur.reznikov@gmail.com>
2015-09-23 23:14:10 -0700

    Update solr.pp

--- solr.pp
+++ solr.pp
@@ -1,15 +1,33 @@
 package {
+      'solr':
+        ensure  => present;
+    }
+
+package {
       'java-1.7.0-openjdk':
         ensure  => present;
     }
 
-    package {
+package {
       'java-1.7.0-openjdk-devel':
         ensure  => present;
     }
 
-    exec {
-      'download and install sorl':
-        command => "/bin/echo 'cd /opt && /usr/bin/wget -O /opt/solr-5.3.0.tgz  http://apache.mirror.gtcomm.net/lucene/solr/5.3.0/solr-5.3.0.tgz && tar -xzf /opt/solr-5.3.0.tgz && rm -f /opt/solr-5.3.0.tgz && ln -s /opt/solr-5.3.0 /opt/solr && mv -v /opt/solr/example /opt/solr/core' | bash",
-        require => Package['java-1.7.0-openjdk'];
-    }
+service { "solr":
+    enable     => true,
+    ensure     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    subscribe  => [ File["/etc/sysconfig/solr"]
+                  ],
+  }
+
+file { "/etc/sysconfig/solr":
+    ensure  => present,
+    owner   => "root",
+    group   => "root",
+    mode    => 0644,
+    content => template("/root/pp/solr/solr.tmpl"),
+    notify  => Service["solr"],
+    require => Package["solr"],
+  }
