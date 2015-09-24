9b7e793fec42df42bfe95886e114b49ee605b73b
arturre <artur.reznikov@gmail.com>
2015-09-23 23:15:07 -0700

    Update activemq.pp

--- activemq.pp
+++ activemq.pp
@@ -1,15 +1,91 @@
 package {
+      'activemq':
+        ensure  => present;
+    }
+
+package {
       'java-1.7.0-openjdk':
         ensure  => present;
     }
 
-    package {
+package {
       'java-1.7.0-openjdk-devel':
         ensure  => present;
     }
 
-    exec {
-      'download  and install activemq':
-        command => "/bin/echo 'cd /opt && /usr/bin/wget -O /opt/apache-activemq-5.12.0-bin.tar.gz  http://apache.mirror.vexxhost.com/activemq/5.12.0/apache-activemq-5.12.0-bin.tar.gz  && tar -xzf /opt/apache-activemq-5.12.0-bin.tar.gz && rm -f apache-activemq-5.12.0-bin.tar.gz  && ln -s /opt/apache-activemq-5.12.0 /opt/activemq' | bash",
-        require => Package['java-1.7.0-openjdk'];
-    }
+service { "activemq":
+    enable     => true,
+    ensure     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    subscribe  => [     File["/etc/sysconfig/activemq"],
+                        File["/etc/activemq/activemq.xml"],
+                        File["/etc/activemq/credentials.properties"],
+                        File["/etc/activemq/jetty-realm.properties"],
+                        File["/etc/activemq/jetty-realm.properties"],
+                        File["/etc/activemq/jetty.xml"],
+                        File["/etc/activemq/log4j.properties"],
+                  ],
+  }
+
+
+file { "/etc/sysconfig/activemq":
+    ensure  => present,
+    owner   => "root",
+    group   => "root",
+    mode    => 0644,
+    content => template("/root/pp/activemq/activemq.tmpl"),
+    notify  => Service["activemq"],
+    require => Package["activemq"],
+  }
+
+
+file { "/etc/activemq/activemq.xml":
+    ensure  => present,
+    owner   => "root",
+    group   => "root",
+    mode    => 0644,
+    content => template("/root/pp/activemq/activemq.xml.tmpl"),
+    notify  => Service["activemq"],
+    require => Package["activemq"],
+  }
+
+file { "/etc/activemq/credentials.properties":
+    ensure  => present,
+    owner   => "root",
+    group   => "root",
+    mode    => 0644,
+    content => template("/root/pp/activemq/credentials.properties.tmpl"),
+    notify  => Service["activemq"],
+    require => Package["activemq"],
+  }
+
+file { "/etc/activemq/jetty-realm.properties":
+    ensure  => present,
+    owner   => "root",
+    group   => "root",
+    mode    => 0644,
+    content => template("/root/pp/activemq/jetty-realm.properties.tmpl"),
+    notify  => Service["activemq"],
+    require => Package["activemq"],
+  }
+
+file { "/etc/activemq/jetty.xml":
+    ensure  => present,
+    owner   => "root",
+    group   => "root",
+    mode    => 0644,
+    content => template("/root/pp/activemq/jetty.xml.tmpl"),
+    notify  => Service["activemq"],
+    require => Package["activemq"],
+  }
+
+file { "/etc/activemq/log4j.properties":
+    ensure  => present,
+    owner   => "root",
+    group   => "root",
+    mode    => 0644,
+    content => template("/root/pp/activemq/log4j.properties.tmpl"),
+    notify  => Service["activemq"],
+    require => Package["activemq"],
+  }
